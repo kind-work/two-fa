54a711a78166ce7fc52aa7ebc0bbe236cd0dbed2
Jonathan Cohlmeyer <jcohlmeyer@jrc9.ca>
2019-12-29 00:03:30 +0000

    Redirect to CP if already authenticated, remove unused imports

--- src/resources/middleware/CheckTwoFa.php
+++ src/resources/middleware/CheckTwoFa.php
@@ -2,17 +2,23 @@
 
 namespace KindWork\TwoFa\Middleware;
 
-use Log;
 use Closure;
 use Illuminate\Http\Request;
-use PragmaRX\Google2FA\Google2FA;
 use Statamic\Facades\User;
-use Statamic\Exceptions\AuthenticationException;
-use Statamic\Exceptions\AuthorizationException;
 
 class CheckTwoFa {   
   public function handle(Request $request, Closure $next) {
     $user = User::current();
+
+    if (
+        // If we are on the 2FA auth route
+        $request->path() == "cp/two-fa" &&
+        // Check to see if we already authed with 2FA on the session
+        $request->session()->get("two_fa_authenticated")
+    ) {
+      // Go to the CP Index
+      return redirect(cp_route("index"));    
+    }
     
     if (
         // make sure we have a user
@@ -24,7 +30,8 @@ class CheckTwoFa {
         // make sure we are not on the 2FA auth route
         $request->path() !== "cp/two-fa" &&
         // make sure we are not already authed with 2FA on the session
-        !$request->session()->get("two_fa_authenticated")) {
+        !$request->session()->get("two_fa_authenticated")
+    ) {
       // If we get here go to the two FA page to get the code
       return redirect(cp_route("two-fa"));
     }
