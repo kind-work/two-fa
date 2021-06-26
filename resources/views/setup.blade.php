353ca9dab3867e0cfa157bc676bf10c1e85a4268
Jonathan Cohlmeyer <jonathan@kind.work>
2021-06-26 07:28:17 -0230

    Improve autocomplete of OTP code. Fixes #15

--- resources/views/setup.blade.php
+++ resources/views/setup.blade.php
@@ -24,7 +24,18 @@
       </div>
       <div class="mb-4">
         <label class="mb-1" for="secret">{{ __("twofa::setup.label") }}</label>
-        <input type="number" class="two-fa-input input-text" name="secret" id="secret" pattern="\d{6}" maxlength="6" minlength="6" step="1" required>
+        <input
+          type="number"
+          class="two-fa-input input-text"
+          name="secret"
+          id="secret"
+          pattern="\d{6}"
+          maxlength="6"
+          minlength="6"
+          step="1"
+          autocomplete="one-time-code"
+          required
+        />
       </div>
       <div class="flex justify-between items-center">
         <button type="submit" class="btn btn-primary">{{ __("twofa::setup.button") }}</button>
